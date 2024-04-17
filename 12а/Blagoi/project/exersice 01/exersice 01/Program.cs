4a98b38c88b37a6406dd19c4723346afcb54626d
blagoi55 <167291963+blagoi55@users.noreply.github.com>
2024-04-17 11:10:57 +0300

    Add files via upload

---
+++ 12а/Blagoi/project/exersice 01/exersice 01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < args.Length; i++) { }   
+
+            
+                   
+        }
+    }  
+}
+
