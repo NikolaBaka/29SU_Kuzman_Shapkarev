1ccaa05b9a19cd25d694a7047da23b158dbf474e
NikolaBaka <139927517+NikolaBaka@users.noreply.github.com>
2024-10-21 13:24:18 +0300

    Add files via upload

---
+++ 11a/EX2/EX2/EX1.cs
@@ -0,0 +1,64 @@
+﻿
+//int n = int.Parse(Console.ReadLine());
+
+//for (int i = 1; i <= n; i++)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write(" * ");
+//    }
+//    Console.WriteLine();
+//}
+
+//for (int i = n; i > 0; i--)
+//{
+//    for (int j = 1; j < i; j++)
+//    {
+//        Console.Write(" * ");
+//    }
+//    Console.WriteLine();
+//}
+
+// Да сравним масивите.
+// да - едакви
+// не - различни
+
+int[] array1 = { 2, 6, 3, 4, 5 };
+int[] array2 = { 2, 6, 3, 4, 5 };
+int j = 0;
+
+if (array1.Length == array2.Length)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i] == array2[i])
+        {
+            j++;
+        }
+        else
+        {
+            Console.WriteLine("No equal");
+            break;
+        }
+    }
+    if (j == array1.Length)
+    {
+        Console.WriteLine("Equal");
+    }
+}
+else
+{
+    Console.WriteLine("No equal");
+}
+
+int sum = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+    sum += array1[i];
+    sum = sum + array1[i];
+}
+
+Console.WriteLine(sum / array1.Length);
+
+
+
