6b35dc83543863c39156f7d0e5d306b782342088
NikolaBaka <139927517+NikolaBaka@users.noreply.github.com>
2024-11-25 14:07:29 +0200

    Add files via upload

---
+++ 11a/Exercises-Solutions/ChageBuro/ChageBuro/ChageBuro/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ChageBuro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lableShow = new Label();
+            textBoxCurrency = new TextBox();
+            buttonCalc = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            groupBox1 = new GroupBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            label1.Location = new Point(332, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 30);
+            label1.TabIndex = 0;
+            label1.Text = "BGN";
+            // 
+            // lableShow
+            // 
+            lableShow.AutoSize = true;
+            lableShow.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            lableShow.Location = new Point(70, 125);
+            lableShow.Name = "lableShow";
+            lableShow.Size = new Size(76, 30);
+            lableShow.TabIndex = 1;
+            lableShow.Text = "label2";
+            // 
+            // textBoxCurrency
+            // 
+            textBoxCurrency.Location = new Point(78, 57);
+            textBoxCurrency.Name = "textBoxCurrency";
+            textBoxCurrency.Size = new Size(248, 31);
+            textBoxCurrency.TabIndex = 2;
+            // 
+            // buttonCalc
+            // 
+            buttonCalc.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            buttonCalc.Location = new Point(332, 176);
+            buttonCalc.Name = "buttonCalc";
+            buttonCalc.Size = new Size(156, 41);
+            buttonCalc.TabIndex = 3;
+            buttonCalc.Text = "Calculate";
+            buttonCalc.UseVisualStyleBackColor = true;
+            buttonCalc.Click += buttonCalc_Click;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(20, 74);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(141, 29);
+            radioButton1.TabIndex = 4;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(20, 30);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(141, 29);
+            radioButton2.TabIndex = 5;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(398, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(221, 150);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(659, 247);
+            Controls.Add(groupBox1);
+            Controls.Add(buttonCalc);
+            Controls.Add(textBoxCurrency);
+            Controls.Add(lableShow);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label lableShow;
+        private TextBox textBoxCurrency;
+        private Button buttonCalc;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private GroupBox groupBox1;
+    }
+}
---
+++ 11a/Exercises-Solutions/FitnessCard/FitnessCard/FitnessCard/Program.cs
@@ -0,0 +1,77 @@
+﻿Console.WriteLine("Enter Balance: ");
+double totalSum = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter Gender: ");
+string gender = Console.ReadLine();
+Console.WriteLine("Enter age: ");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Sport: ");
+string sport = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine("Summary: ");
+
+double sportPrice;
+double[] malePrices = { 42, 41, 45, 34, 51, 39 };
+double[] femalePrices = { 35, 37, 42, 31, 53, 37 };
+
+void conditions (int age, double totalSum, double sportPrice)
+{
+    if (age > 19)
+    {
+        if (totalSum >= sportPrice)
+        {
+            Console.WriteLine($"You purchased {Math.Round(totalSum / sportPrice, 0)} months");
+        }
+        else
+        {
+            Console.WriteLine("You do not have enough money!");
+        }
+
+    }
+    else
+    {
+        if (totalSum >= sportPrice)
+        {
+            Console.WriteLine($"You purchased {Math.Round(totalSum / (sportPrice - ((20 / 100) * sportPrice)), 0)} months");
+        }
+        else
+        {
+            Console.WriteLine("You do not have enough money!");
+        }
+    }
+}
+void findingCardPrices(double totalSum, string gender, int age, double sportPriceF, double sportPriceM)
+{
+    if (gender == "f")
+    {
+        conditions(age, totalSum, sportPriceF);
+
+    }
+    else if (gender == "m")
+    {
+        conditions(age, totalSum, sportPriceM);
+    }
+}
+
+
+switch (sport)
+{
+    case "Gym":
+        findingCardPrices(totalSum, gender, age, femalePrices[0], malePrices[0]);
+        break;
+    case "Boxing":
+        findingCardPrices(totalSum, gender, age, femalePrices[1], malePrices[1]);
+        break;
+    case "Yoga":
+        findingCardPrices(totalSum, gender, age, femalePrices[2], malePrices[2]);
+        break;
+    case "Zumba":
+        findingCardPrices(totalSum, gender, age, femalePrices[3], malePrices[3]);
+        break;
+    case "Dances":
+        findingCardPrices(totalSum, gender, age, femalePrices[4], malePrices[4]);
+        break;
+    case "Pilates":
+        findingCardPrices(totalSum, gender, age, femalePrices[5], malePrices[5]);
+        break;
+}
